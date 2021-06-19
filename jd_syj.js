6645414b53b46a9e6b8f0b69e62f542fce92db21
hajiuhajiu02 <85703748+hajiuhajiu02@users.noreply.github.com>
2021-06-19 13:28:29 +0800

    Update jd_syj.js

--- jd_syj.js
+++ jd_syj.js
@@ -51,7 +51,7 @@ const JD_API_HOST = 'https://api.m.jd.com/api';
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', {"open-url": "https://bean.m.jd.com/bean/signIndex.action"});
     return;
   }
-  await getAuthorShareCode('https://raw.githubusercontent.com/hajiuhajiu/jdsign1112/master/jd_zz.json');
+  await getAuthorShareCode('https://cdn.jsdelivr.net/gh/1277002811/JDbot@master/shareCodes/jd_zz.json');
   for (let i = 0; i < cookiesArr.length; i++) {
     if (cookiesArr[i]) {
       cookie = cookiesArr[i];
