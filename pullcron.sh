bb5cc03f0b908b1cd4f4b21e10fa0eae634e0f30
hajiuhajiu02 <85703748+hajiuhajiu02@users.noreply.github.com>
2021-06-20 13:25:59 +0800

    Update pullcron.sh

--- pullcron.sh
+++ pullcron.sh
@@ -23,8 +23,8 @@ ContentNewTask=${ShellDir}/new_task
 ContentDropTask=${ShellDir}/drop_task
 SendCount=${ShellDir}/send_count
 isTermux=${ANDROID_RUNTIME_ROOT}${ANDROID_ROOT}
-ShellURL=https://ghproxy.com/https://github.com/wuzhi04/jd-base
-ScriptsURL=https://ghproxy.com/https://github.com/wuzhi04/MyActions
+ShellURL=https://ghproxy.com/https://github.com/hajiuhajiu02/jd-shell
+ScriptsURL=https://ghproxy.com/https://github.com/hajiuhajiu02/scripts
 
 ## 更新crontab，gitee服务器同一时间限制5个链接，因此每个人更新代码必须错开时间，每次执行git_pull随机生成。
 ## 每天次数随机，更新时间随机，更新秒数随机，至少6次，至多12次，大部分为8-10次，符合正态分布。
@@ -41,27 +41,11 @@ function Git_PullShell {
   cd ${ShellDir}
   git fetch --all
   ExitStatusShell=$?
-  git reset --hard origin/main
+  git reset --hard origin/master
   echo
 }
 
-## 克隆scripts
-function Git_CloneScripts {
-  echo -e "克隆脚本\n"
-  git clone -b main ${ScriptsURL} ${ScriptsDir}
-  ExitStatusScripts=$?
-  echo
-}
 
-## 更新scripts
-function Git_PullScripts {
-  echo -e "更新脚本\n"
-  cd ${ScriptsDir}
-  git fetch --all
-  ExitStatusScripts=$?
-  git reset --hard origin/main
-  echo
-}
 
 ## 更新docker-entrypoint
 function Update_Entrypoint {
