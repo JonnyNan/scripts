120f412adac07120e180fdabed9e6ca2cb7550ca
hajiuhajiu <2705322732@qq.com>
2021-07-01 05:48:37 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -2,6 +2,7 @@ ENV_PATH=
 #0622 更新scripts目录wyw
 # 每3天的23:50分清理一次日志(互助码不清理，proc_file.sh对该文件进行了去重)
 50 23 */3 * * find /scripts/logs -name '*.log' | grep -v 'sharecodeCollection' | xargs rm -rf
+50 23 */3 * * find /log -name '*.log' | grep -v 'sharecodeCollection' | xargs rm -rf
 #收集助力码
 30 * * * * sh +x /scripts/docker/auto_help.sh collect >> /scripts/logs/auto_help_collect.log 2>&1
 
@@ -29,7 +30,6 @@ ENV_PATH=
 33 0-23/4 * * * bash MY_PATH/jd.sh jd_bean_home.js >> /scripts/logs/jd_bean_home.log 2>&1
 0 0 * * * bash MY_PATH/jd.sh jd_bean_sign.js >> /scripts/logs/jd_bean_sign.log 2>&1
 1 7,12,19 * * * bash MY_PATH/jd.sh jd_beauty.js >> /scripts/logs/jd_beauty.log 2>&1
-0 * * * * bash MY_PATH/jd.sh jd_big_winner.js >> /scripts/logs/jd_big_winner.log 2>&1
 0 0 * * * bash MY_PATH/jd.sh jd_blueCoin.js >> /scripts/logs/jd_blueCoin.log 2>&1
 0 0 * * * bash MY_PATH/jd.sh jd_blueCoin20.js >> /scripts/logs/jd_blueCoin20.log 2>&1
 7 8,12,18 * * * bash MY_PATH/jd.sh jd_bookshop.js >> /scripts/logs/jd_bookshop.log 2>&1
