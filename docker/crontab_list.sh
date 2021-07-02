285aafc36e4c3f33e8e3fb66fa0fd23fb0d64187
hajiuhajiu <2705322732@qq.com>
2021-07-03 05:11:49 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -112,7 +112,7 @@ ENV_PATH=
 1 0 * * * bash /root/jd/jd.sh jx_cfdtx.js >> /log/jx_cfdtx.log 2>&1
 18 3 * * * bash /root/jd/jd.sh jx_sign.js >> /log/jx_sign.log 2>&1
 0 0-23/1 * * * bash /root/jd/jd.sh jd_super_redrain.js >> /log/jd_super_redrain.log 2>&1
-22 3 * * * bash /root/jd/jd.sh jd_tyt.js >> /log/jd_tyt.log 2>&1
+
 #东东电竞经理 格式ok
 22 22 * * * bash /root/jd/jd.sh jd_djjl
 29 3 * * * bash /root/jd/jd.sh jd_ddo_pk.js >> /log/jd_ddo_pk.log 2>&1
