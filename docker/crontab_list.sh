5fa7d6e7cc3dcedc0d2fbbac4acfd87e14c723a1
hajiuhajiu <2705322732@qq.com>
2021-07-06 15:48:56 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -111,10 +111,8 @@ ENV_PATH=
 
 #东东电竞经理 格式ok
 22 22 * * * bash /root/jd/jd.sh jd_djjl
-29 3 * * * bash /root/jd/jd.sh jd_ddo_pk.js >> /log/jd_ddo_pk.log 2>&1
 33 6,8 * * * bash /root/jd/jd.sh jd_europeancup.js >> log/jd_europeancup.log 2>&1
 13 6,8 * * * bash /root/jd/jd.sh jd_paoku
-23 6,8 * * * bash /root/jd/jd.sh jd_qqxing
 15 0-23/4 * * * bash /root/jd/jd.sh jd_jxmc
 29 4 * * * bash /root/jd/jd.sh jd_ddnc_farmpark
 1 0,1 * * * node /scripts/jd_qmqjd.js >> log/jd_qmqjd.log 2>&1
@@ -122,6 +120,8 @@ ENV_PATH=
 2 0,2 * * * node /scripts/jd_zjb.js >> /log/jd_zjb.log 2>&1
 18 6,9 * * * node /scripts/jd_superBrand.js >> log/jd_superBrand.log 2>&1
 20 8,10 * * * node /scripts/jd_DrawEntrance.js >> log/jd_DrawEntrance.log 2>&1
+5 9,11 * * * node /scripts/jd_ddo_pk.js >> /log/jd_ddo_pk.log 2>&1
+23 6,8 * * * node /scripts/jd_qqxing.js >> /log/jd_qqxing.log 2>&1
 
 
 
