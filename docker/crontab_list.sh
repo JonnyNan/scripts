444a103aa7cc63ff8900c3a3b5c4760c2b74e6bb
hajiuhajiu <2705322732@qq.com>
2021-07-14 15:27:59 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -133,6 +133,7 @@ ENV_PATH=
 53 4,6,15 * * * node /scripts/MovementFaker.js >> /log/MovementFaker.log 2>&1
 02 9,18,20 * * * node /scripts/jd_summer_movement_help.js >> /log/jd_summer_movement_help.log 2>&1
 06 0,1,10 * * * node /scripts/jd_sendBeans.js >> /log/jd_sendBeans.log 2>&1
+12 */6 * * * node /scripts/jd_cfd_loop.js >> /log/jd_cfd_loop.log 2>&1
 
 
 
