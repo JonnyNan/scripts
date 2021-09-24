70a0207d9e5e98141d686c4df8e06f1c3ee6d5db
hajiuhajiu <2705322732@qq.com>
2021-09-24 15:37:55 +0800

    Update jd_cfd_mooncake.js

--- jd_cfd_mooncake.js
+++ jd_cfd_mooncake.js
@@ -60,11 +60,11 @@ $.appId = 10028;
   $.CryptoJS = $.isNode() ? require('crypto-js') : CryptoJS;
   await requestAlgo();
   await $.wait(1000)
-  let res = await getAuthorShareCode('https://gitee.com/xr2021/share/raw/master/cfd.json')
+  let res = await getAuthorShareCode('https://xr2021.coding.net/p/import-kasd/d/JDbot/git/raw/master/shareCodes/cfd.json')
   if (!res) {
-    $.http.get({url: 'https://gitee.com/xr2021/share/raw/master/cfd.json'}).then((resp) => {}).catch((e) => console.log('刷新CDN异常', e));
+    $.http.get({url: 'https://xr2021.coding.net/p/import-kasd/d/JDbot/git/raw/master/shareCodes/cfd.json'}).then((resp) => {}).catch((e) => console.log('刷新CDN异常', e));
     await $.wait(1000)
-    res = await getAuthorShareCode('https://gitee.com/xr2021/share/raw/master/cfd.json')
+    res = await getAuthorShareCode('https://xr2021.coding.net/p/import-kasd/d/JDbot/git/raw/master/shareCodes/cfd.json')
   }
   $.strMyShareIds = [...(res && res.shareId || [])]
   for (let i = 0; i < cookiesArr.length; i++) {
