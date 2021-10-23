53aa94428d0abd0ba6bdf4d02e7305fe4d7d9cd3
hajiuhajiu <2705322732@qq.com>
2021-10-24 06:31:50 +0800

    Update jd_speed_sign.js

--- jd_speed_sign.js
+++ jd_speed_sign.js
@@ -93,7 +93,7 @@ async function jdGlobal() {
     await apTaskList()
     await wheelsHome()
     if ($.canhelp) {
-      console.log(`\n京东账号${$.index}开始助力【zero205】邀请有礼，感谢！\n`);
+      //console.log(`\n京东账号${$.index}开始助力【zero205】邀请有礼，感谢！\n`);
       await invite()
       await invite2()
     }
