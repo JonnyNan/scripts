c1f09a8700f000dfe7a40ba2f3a38e3621fc813a
hajiuhajiu <2705322732@qq.com>
2021-10-30 20:43:15 +0800

    Update jd_redEnvelope.js

--- jd_redEnvelope.js
+++ jd_redEnvelope.js
@@ -8,7 +8,7 @@ https://u.jd.com/3tVMzEv
 const $ = new Env('双十一无门槛红包🧧');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 const notify = $.isNode() ? require('./sendNotify') : '';
-const Faker = require('./utils/sign_graphics_validate.js')
+const Faker = require('./sign_graphics_validate.js')
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [],
   cookie = '';
