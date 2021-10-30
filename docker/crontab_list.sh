6d97be70dc18c83392ede676f7cd3559bff2c680
hajiuhajiu <2705322732@qq.com>
2021-10-30 12:30:37 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -164,6 +164,12 @@ ENV_PATH=
 32 */6 * * * node /scripts/jd_fcdyj.js >> /log/jd_fcdyj.log 2>&1
 02 1,4,10,12 * * * node /scripts/jd_qjd.js >> /log/jd_qjd.log 2>&1
 02 0,12,18,20 * * * node /scripts/jd_redEnvelope.js >> /log/jd_redEnvelope.log 2>&1
+15 4,8,13,15 * * * node /scripts/jd_lol.js >> /log/jd_lol.log 2>&1
+25 5,8,13,17 * * * node /scripts/jd_jddj_bean.js >> /log/jd_jddj_bean.log 2>&1
+45 6,8,13,15 * * * node /scripts/jd_jddj_fruit.js >> /log/jd_jddj_fruit.log 2>&1
+55 7,10,13,16 * * * node /scripts/jd_jddj_fruit_collectWater.js >> /log/jd_jddj_fruit_collectWater.log 2>&1
+05 1,8,13,20 * * * node /scripts/jd_jddj_getPoints.js >> /log/jd_jddj_getPoints.log 2>&1
+18 3,8,13,17 * * * node /scripts/jd_jddj_plantBeans.js >> /log/jd_jddj_plantBeans.log 2>&1
 
 
 
