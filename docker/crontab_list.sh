e146d3d705b1fd6d60ba97b8fa3d80d7fffd581b
hajiuhajiu <2705322732@qq.com>
2021-11-03 15:59:58 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -174,6 +174,9 @@ ENV_PATH=
 3,15 20 31 10 * node /scripts/jd_hotNeight.js >> /log/jd_hotNeight.log 2>&1
 01 0,6,12,20 * * * node /scripts/jd_hello_wind_xtg.js >> /log/jd_hello_wind_xtg.log 2>&1
 03 0,6,12,20 * * * node /scripts/jd_xtgsign.js >> /log/jd_xtgsign.log 2>&1
+26 0,9,18,23 * * * node /scripts/jd_nnfls.js >> /log/jd_nnfls.js 2>&1
+
+
 
 
 
