f0f821c27527f80e4da7ae4d46bf6f407a41207c
hajiuhajiu <2705322732@qq.com>
2021-11-06 14:25:04 +0800

    Add files via upload

--- jd_carnivalcity.js
+++ jd_carnivalcity.js
@@ -36,7 +36,7 @@ if ($.isNode()) {
     cookiesArr.push(jdCookieNode[item])
   })
   if (process.env.JD_DEBUG && process.env.JD_DEBUG === 'false') console.log = () => {};
-  
+  if (JSON.stringify(process.env).indexOf('GITHUB') > -1) process.exit(0)
 } else {
   cookiesArr = [$.getdata('CookieJD'), $.getdata('CookieJD2'), ...jsonParse($.getdata('CookiesJD') || "[]").map(item => item.cookie)].filter(item => !!item);
 }
@@ -142,7 +142,7 @@ function headInfo() {
                 if (data.data.taskType === "13" || data.data.taskType === "15") {
                   console.log(`开始 【顶部】浏览任务,需等待6秒`)
                   await doBrowseHead(data.data.taskIndex, data.data.taskId, data.data.taskType)
-                } else if (data.data.taskType === "14") {
+                } else if (data.data.taskType === "14" && ["car","card"].includes(process.env.FS_LEVEL)) {
                   console.log(`开始 【顶部】加购任务`)
                   await getHeadJoinPrize(data.data.taskId, data.data.taskIndex)
                 }
