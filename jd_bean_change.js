4e04d0086bb1a9304947d8a88879a5fa9a43bcec
hajiuhajiu <2705322732@qq.com>
2021-12-20 13:20:59 +0800

    Update jd_bean_change.js

--- jd_bean_change.js
+++ jd_bean_change.js
@@ -20,6 +20,7 @@ let allWarnMessage = '';
 let ReturnMessage = '';
 let ReturnMessageMonth = '';
 let allMessageMonth = '';
+let intPerSent  = 5;
 
 let MessageUserGp2 = '';
 let ReceiveMessageGp2 = '';
