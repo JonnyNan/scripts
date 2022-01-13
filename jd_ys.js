101a131d0889ca64440a1ec3be62ec7d172fd332
hajiuhajiu <2705322732@qq.com>
2022-01-13 08:46:42 +0800

    Update jd_ys.js

--- jd_ys.js
+++ jd_ys.js
@@ -22,7 +22,7 @@ if ($.isNode()) {
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 
-$.inviteId  = ['m2telgJiXU4EtwCgJrnbN5TV6R25CgqUPfjsarOw7A'];
+$.inviteId  = ['m2telgJiXU4EtwCgJrnbN5TV6R25CgqUPfjsarOw7Q'];
 
 !(async () => {
   if (!cookiesArr[0]) {
