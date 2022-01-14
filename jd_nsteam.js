67bb6ff8bdebde542dd7e273fae37e1407e12c02
hajiuhajiu <2705322732@qq.com>
2022-01-14 09:37:01 +0800

    Update jd_nsteam.js

--- jd_nsteam.js
+++ jd_nsteam.js
@@ -22,7 +22,7 @@ if ($.isNode()) {
 }
 const JD_API_HOST = 'https://api.m.jd.com/client.action';
 
-$.inviteId  = ["m2telgJiXU4EtwCgJrnbN5TV6R25CgqUPfjsarOw7Q","m2telgJiREMGkCOmVcmzI764C1g3JwJ2tanncL6f_KrZ98pGGh0G6C0"];
+$.inviteId  = ["m2telgJiXU4EtwCgJrnbN5TV6R25CgqUPfjsarOw7g","m2telgJiXU4EtwCgJrnbN5TV6R25CgqUPfjsarOw7g"];
 
 !(async () => {
   if (!cookiesArr[0]) {
